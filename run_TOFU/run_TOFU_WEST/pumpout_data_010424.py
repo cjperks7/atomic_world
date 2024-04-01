5a7f28249ad5bb46b84453baa002702582c2088e
cjperks7 <cjperks@mit.edu>
2024-04-01 19:40:46 +0200

    Init using tofu_west

---
+++ run_TOFU/run_TOFU_WEST/pumpout_data_010424.py
@@ -0,0 +1,12 @@
+'''
+
+pumpout_dat_040124.py is a test script to test my understanding of using
+the tofu_west module to load XICS data
+
+cjperks
+April 1, 2024
+
+'''
+
+# Modules
+import tofU_west as tfw
