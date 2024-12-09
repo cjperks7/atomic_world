0313316eb1d7bd9ff88fe0fe89fe60569d5d7f2f
cjperks7 <cjperk@mit.edu>
2024-12-09 17:09:35 -0500

    added normalizing by exposure time

--- read_LLNL_EBIT/main/read_histos.py
+++ read_LLNL_EBIT/main/read_histos.py
@@ -16,7 +16,11 @@ from atomic_world.run_SCRAM.scripts.tests import _read_sHANSEN as rh
 import matplotlib.gridspec as gridspec
 import scipy.constants as cnt
 from matplotlib.widgets import Slider
+from matplotlib.ticker import ScalarFormatter
+
+sys.path.insert(0, '/home/cjperks/tofu_sparc')
 from Pinhole_Optimization import _get_transmission
+sys.path.pop(0)
 
 # Useful constants
 hc = cnt.h*cnt.c/cnt.e*1e10 # [eV*AA]
@@ -69,6 +73,9 @@ def read_histos(
             '8.45',
             '8.71',
             '8.97',
+            '9.23',
+            '9.49',
+            '9.75',
             '15(highCB)',
             '15(lowCB)'
             ]
@@ -88,7 +95,8 @@ def read_histos(
 
         specs[key] = _load_histo_ascii(
             file_path = file_path,
-            name = name
+            name = name,
+            key = key,
             )
 
     # Output
@@ -97,7 +105,8 @@ def read_histos(
 # Loads histogram ascii files
 def _load_histo_ascii(
     file_path = None,
-    name = None
+    name = None,
+    key = None,
     ):
 
     # Init
@@ -117,11 +126,11 @@ def _load_histo_ascii(
                 float(line.split()[1])
                 )
             out['hist_all'].append(
-                float(line.split()[2])
-                )
+                float(line.split()[2])/_get_exposure_time(key=key)
+                ) # [counts/s]
             out['hist'].append(
-                float(line.split()[3])
-                )
+                float(line.split()[3])/_get_exposure_time(key=key)
+                ) # [counts/s]
 
     ff.close()
 
@@ -133,6 +142,85 @@ def _load_histo_ascii(
 
     # Output
     return out
+
+# Spectrum exposure time
+def _get_exposure_time(key=None):
+    dt = {
+        '3.93 kV': (
+            (-31.602224 +38.010864)
+            +(-38.197735 +39.000191)+(-43.429588 +44.003353)    # d0523
+            )*1e3, # [s]
+        '4.04 kV': (
+            (-44.056202 +47.808311)                             # d0523
+            )*1e3, # [s]
+        '7.15 kV': (
+            (-40.545834 +48.577091)                             # d0521
+            +(-11.350809 +33.949932)                            # d0522
+            )*1e3, # [s]
+        '7.28 kV': (
+            (-34.013535 +46.902763)                             # d0522
+            +(-7.7139196 +14.414927)                            # d0523
+            )*1e3, # [s]
+        '7.41 kV': (
+            (-14.457975 +29.336212)                             # d0523
+            )*1e3, # [s]
+        '7.54 kV': (
+            (-0.94638497 +17.092041)                            # d0524b
+            )*1e3, # [s]
+        '7.67(highCB) kV': (
+            (-9.5525484 +23.922386)                             # d0528
+            )*1e3, # [s]
+        '7.67(lowCB) kV': (
+            (-17.23085 +35.139351)                              # d0524b
+            )*1e3, # [s]
+        '7.93 kV': (
+            (-24.180567 +37.828835)                             # d0528
+            )*1e3, # [s]
+        '8.19 kV': (
+            (-11.704685 +14.082019) +(-14.702413 +15.207074)
+            +(-16.833208 +38.646503)                            # d0529
+            )*1e3, # [s]
+        '8.27 kV': (
+            (-6.667768 +29.659929)                              # d0531
+            )*1e3, # [s]
+        '8.45 kV': (
+            (-6.2088385 +26.041937)                             # d0530
+            )*1e3, # [s]
+        '8.71 kV': (
+            (-26.219839 +28.458445) +(-33.077572 +45.6054)      # d0530
+            )*1e3, # [s]
+        '8.97 kV': (
+            (-29.740816 +47.80595)                              # d0531
+            )*1e3, # [s]
+        '9.23 kV': (
+            (-15.464238 +36.419247)                             # d0604
+            )*1e3, # [s]
+        '9.49 kV': (
+            (-37.310993 +42.007515)+(-42.433636 +46.376831)     # d0604
+            +(-10.150639 +21.215521)                            # d0605
+            )*1e3, # [s]
+        '9.75 kV': (
+            (-21.34424 +45.729099)                              # d0605
+            )*1e3, # [s]
+        '15(highCB) kV': (
+            (-11.878275 +15.119467) +(-15.298428 +38.227188)    # d0521
+            +(-5.633327 +11.254564)                             # d0522
+            +(-4.1040225 +7.5842228)                            # d0523
+            +(-4.4182196 +7.67)                                 # d0524a
+            +(-0.0 +0.87047714)                                 # d0524b
+            +(-6.7292438 +9.2936954)                            # d0528
+            +(-6.012445 +11.00299)                              # d0529
+            +(-2.225487 +5.9452343)                             # d0530
+            +(-1.1761446 +6.5206709)                            # d0531
+            )*1e3, # [s]
+        '15(lowCB) kV': (
+            (-6.5399103 +10.22652)+(-11.348508 +38.509972)   # d0520
+            +(-5.3163538 +11.659544)                         # d0521
+            )*1e3, # [s]
+        }
+
+    # Output
+    return dt[key]
 
 # Be window thickness
 def _get_Be(key=None):
@@ -153,6 +241,9 @@ def _get_Be(key=None):
         '8.45 kV': 5.5*mil2meter, # [m]
         '8.71 kV': 5.5*mil2meter, # [m]
         '8.97 kV': 5.5*mil2meter, # [m]
+        '9.23 kV': 5.5*mil2meter, # [m]
+        '9.49 kV': 5.5*mil2meter, # [m]
+        '9.75 kV': 5.5*mil2meter, # [m]
         '15(highCB) kV': 5.5*mil2meter, # [m]
         '15(lowCB) kV': 0, # [m]
         }
@@ -650,6 +741,291 @@ def plot_histos(
     # Attach the update function to sliders
     slider1.on_changed(sliders_on_changed)
     slider2.on_changed(sliders_on_changed)
+
+    # Output
+    return specs, scram
+
+
+def plot_side_by_side(
+    specs = None,
+    scram = None,
+    # Figure controls
+    font_size = 16,
+    fig_size = (16,8),
+    title = 'LLNL EBIT ECS spectra (N.Hell, 06/01/24)',
+    # Axis controls
+    plt_keys = None,
+    x_units = 'eV',
+    x_lim = [[2000,3000], [4500, 4580]],
+    # Color contols
+    spec_map = 'winter',
+    # Slider controls
+    scram_method = 'Max', # 'Max or 'cs'
+    cs_init = 28,
+    Te_init = 5, # [keV]
+    # Extra
+    x_line = None,
+    x_highlight = None,
+    dE = 0.0,
+    plt_scram = [True, False],
+    ):
+
+    # Default values
+    if specs is None:
+        print('Loading histos')
+        specs = read_histos()
+        print('Done')
+
+    # Loads SCRAM data files
+    if scram is None:
+        print('Loading SCRAM')
+        scram = read_SCRAM()
+        print('Done')
+
+    # Init figure
+    plt.rcParams.update({'font.size': font_size})
+    fig = plt.figure(figsize = fig_size)
+    gs = gridspec.GridSpec(
+        1, 
+        3, 
+        width_ratios= [3,3,1]
+        )
+
+    # Init colors
+    colormap = plt.get_cmap(spec_map)
+    colors = colormap(np.linspace(0,1,len(plt_keys)))
+
+    # Init axis labels
+    if x_units == 'eV':
+        xlabel = 'Photon energy [eV]'
+    elif x_units == 'AA':
+        xlabel = r'Photon wavelength, $\lambda$ [$\AA$]'
+
+    # Init axes
+    axs_l = []
+    axs_r = []
+    for ii in np.arange(2):
+        if ii >0:
+            axs_l.append(fig.add_subplot(gs[0,ii]))
+        else:
+            axs_l.append(fig.add_subplot(gs[0,0]))
+
+        axs_r.append(axs_l[-1].twinx())
+
+        axs_l[ii].set_xlabel(xlabel)
+    fig.suptitle(title)
+
+    # Loop over axes
+    for ii in np.arange(2):
+        ### --- Extras --- ###
+        if x_line is not None:
+            for xx in x_line:
+                axs_l[ii].plot(
+                    [xx,xx],
+                    [0, 1e4],
+                    'm--'
+                    )
+
+        if x_highlight is not None:
+            for xxs in x_highlight:
+                axs_l[ii].axvspan(
+                    xxs[0],
+                    xxs[1],
+                    color = 'm',
+                    alpha = 0.3
+                    )
+
+        ### --- Histogram Data --- ###
+
+        # Useful data
+        mspec = 0
+
+        # Loop over spectra to plot
+        for jj, kk in enumerate(plt_keys):
+            # Prepares x-data
+            if x_units == 'eV':
+                bins = np.where(
+                    (specs[kk]['binlow'] >= x_lim[ii][0])
+                    & (specs[kk]['binlow'] <= x_lim[ii][1])
+                    )[0]
+                xbin = specs[kk]['binlow']
+
+                xscram = scram['indspec']['E_eV']['data'] -dE
+                tscram = np.where(
+                    (scram['indspec']['E_eV']['data'] >= x_lim[ii][0])
+                    & (scram['indspec']['E_eV']['data'] <= x_lim[ii][1])
+                    )[0]
+            elif x_units == 'AA':
+                bins = np.where(
+                    (specs[kk]['binlow'] >= hc/x_lim[ii][1])
+                    & (specs[kk]['binlow'] <= hc/x_lim[ii][0])
+                    )[0]
+                xbin = hc/specs[kk]['binlow']
+
+                xscram = hc/(scram['indspec']['E_eV']['data']-dE)
+                tscram = np.where(
+                    (scram['indspec']['lambda_A'] >= x_lim[ii][0])
+                    & (scram['indspec']['lambda_A'] <= x_lim[ii][1])
+                    )[0]
+
+            # Get Be window filtering
+            fil_Be = _get_transmission(
+                key = 'Be',
+                E = scram['indspec']['E_eV']['data'], # [eV]
+                thick = _get_Be(key=kk), # [m]
+                ) # dim(nlambda,)
+
+            # Plots
+            axs_l[ii].step(
+                xbin,
+                specs[kk]['hist'],
+                color = colors[jj],
+                label = kk
+                )
+
+            mspec = np.max((mspec, np.max(specs[kk]['hist'][bins])))
+
+        # Y-label
+        if ii == 0:
+            axs_l[ii].set_ylabel('Counts/dt [ph/s]')
+
+        # Axis limits
+        axs_l[ii].set_xlim(x_lim[ii][0], x_lim[ii][1])
+        axs_l[ii].set_ylim(0,1.2*mspec)
+
+        # Prepares y-axis
+        axs_l[ii].grid('on')
+        axs_l[ii].yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+        axs_l[ii].ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+
+        axs_l[ii].spines['left'].set_color(colors[0])
+        axs_l[ii].yaxis.label.set_color(colors[0])
+        axs_l[ii].tick_params(axis='y', color=colors[0])
+        for ll in axs_l[ii].get_yticklabels():
+            ll.set_color(colors[0])
+        axs_l[ii].yaxis.get_offset_text().set_color(colors[0])
+        
+
+        # Legend
+        if ii == 0:
+            axs_l[ii].legend(
+                loc = 'upper right',
+                #bbox_to_anchor = (1,0.5),
+                labelcolor='linecolor'
+                )
+
+
+        ### --- SCRAM data --- ###
+
+        # Color axis
+        scr_lim = 2
+        axs_r[ii].spines['right'].set_color('red')
+        axs_r[ii].spines['left'].set_color(colors[0])
+        if ii == 1:
+            axs_r[ii].set_ylabel('SCRAM emissivity [arb]')
+        axs_r[ii].yaxis.label.set_color('red')
+        axs_r[ii].tick_params(axis='y', color='red')
+        for ll in axs_r[ii].get_yticklabels():
+            ll.set_color('red')
+        axs_r[ii].set_ylim(0, 1.2*scr_lim)
+
+        if not plt_scram[ii]:
+            continue
+
+        # Just plot the Maxwellian envolope
+        if scram_method == 'Max':
+            # Finds SCRAM spectra to plot
+            scr_ind = np.argmin(
+                abs(scram['table']['Te_eV']['data']/1e3 - Te_init)
+                )
+            scr_kk = 'spec_%i'%(scr_ind)
+
+            scr_data = (
+                scram['indspec'][scr_kk]['j(ph/ion/s/AA)']
+                * scram['indspec'][scr_kk]['abund'][:,None]
+                /scram['table']['ne_cm3']['data'][0]
+                * ne_cm3
+                * fi * ne_cm3
+                ) # dim(ncs, nlambda)
+            scr_data *= fil_Be[None,:]
+
+            mm1 = np.max(scr_data[:,tscram], axis = 1)
+            mm2 = np.argsort(mm1)[-10:][::-1]
+
+            scr_lim = 1
+
+            # Loop over charge states
+            for jj in np.arange(scr_data.shape[0]):
+                if scram['indspec'][scr_kk]['ions'][jj].split('-')[0] == 's':
+                    continue
+                if jj in mm2 and plt_scram:
+                    scr_lim = np.max((scr_lim, np.max(scr_data[jj,tscram])))
+                    axs_r[ii].plot(
+                        xscram,
+                        scr_data[jj,:],
+                        label = _cs_label(
+                            cs = scram['indspec'][scr_kk]['ions'][jj].split('-')[1]
+                            )
+                        )
+        
+        # Just plot the most abundant charge states
+        elif scram_method == 'cs':
+            # Finds SCRAM run where each charge state was maximized
+            css = {}
+            for cs in range(32,5,-1):
+                css[cs] = {
+                    'abund': 0,
+                    'spec': -1,
+                    'ind': -1,
+                    }
+                for tt in range(scram['table']['Te_eV']['data'].shape[0]):
+                    if 'd-%02i'%(cs) in scram['indspec']['spec_%i'%(tt)]['ions']:
+                        ind = scram['indspec']['spec_%i'%(tt)]['ions'].index('d-%02i'%(cs))
+
+                        if scram['indspec']['spec_%i'%(tt)]['abund'][ind] > css[cs]['abund']:
+                            css[cs]['abund'] = scram['indspec']['spec_%i'%(tt)]['abund'][ind]
+                            css[cs]['spec'] = tt
+                            css[cs]['ind'] = ind
+
+            plt_cs = list(range(cs_init-4, cs_init+6))
+
+            for cs in plt_cs:
+                if cs not in css.keys():
+                    continue
+
+                scr_data = (
+                    scram['indspec']['spec_%i'%(css[cs]['spec'])]['j(ph/ion/s/AA)'][css[cs]['ind'],:]
+                    ).copy()
+                #scr_data *= fil_Be
+                scr_data /= np.max(scr_data[tscram])
+
+                if np.mean(scr_data[tscram]) >= 0.1*np.max(scr_data[tscram]):
+                    scr_data = np.zeros(len(scr_data))
+
+                axs_r[ii].plot(
+                    xscram,
+                    scr_data,
+                    label = _cs_label(cs=str(cs))
+                    )
+
+
+    plt.tight_layout()
+
+    if np.any(plt_scram):
+        ind = plt_scram.index(True)
+
+        # Create a separate axis for the legend (row 0, column 1) 
+        legend_ax = fig.add_subplot(gs[0, 2]) 
+        legend_ax.axis('off') 
+        # Hide the axis
+        # Add the legend to the new axis 
+        legend_ax.legend(
+            *axs_r[ind].get_legend_handles_labels(), 
+            loc='center',
+            labelcolor='linecolor'
+            )
+
+        
 
     # Output
     return specs, scram
--- read_LLNL_EBIT/scripts/240607_plt_sliders.py
+++ read_LLNL_EBIT/scripts/240607_plt_sliders.py
@@ -12,8 +12,8 @@ June 7th, 2024
 from atomic_world.read_LLNL_EBIT.main import read_histos as rh
 
 # Enables automatic reloading of modules
-%reload_ext autoreload
-%autoreload 2
+#%reload_ext autoreload
+#%autoreload 2
 
 # Make histogram plots
 specs, scram = rh.plot_histos(
@@ -21,6 +21,7 @@ specs, scram = rh.plot_histos(
     #dE = 5, # [eV]
     scram_method = 'cs',
     num_ax = 4,
+    spec_init = 15
     )
 
 specs, scram = rh.plot_histos(
@@ -30,7 +31,7 @@ specs, scram = rh.plot_histos(
     x_line = [2179, 2459, 2595],
     #Te_init = 10.0,
     cs_init = 14,
-    spec_init = 14,
+    spec_init = 11,
     #plt_scram = True,
     scram_method = 'cs',
     num_ax = 4,
@@ -39,16 +40,17 @@ specs, scram = rh.plot_histos(
 specs, scram = rh.plot_histos(
     specs=specs,
     scram=scram,
-    x_units = 'AA',
-    x_lim = [2.7, 2.78],
-    x_line = [2.72, 2.729, 2.736, 2.747],
-    x_highlight = [[2.71, 2.755]],
-    #x_lim = [4400, 5000],
-    #x_line = [4560],
-    #x_highlight = [[4500,4575]],
+    #x_units = 'AA',
+    #x_lim = [2.7, 2.78],
+    #x_line = [2.72, 2.729, 2.736, 2.747],
+    #x_highlight = [[2.71, 2.755]],
+    x_lim = [4450, 4600],
+    x_line = [4560, 4543, 4531, 4513],
+    x_highlight = [[4500,4575]],
     scram_method = 'cs',
-    cs_init = 15,
-    spec_init = 14,
+    #cs_init = 14,
+    cs_init = 23,
+    spec_init = 11,
     num_ax = 4,
     )
 
@@ -59,6 +61,235 @@ specs, scram = rh.plot_histos(
     #x_lim = [7600,8200],
     scram_method='cs',
     cs_init = 14,
+    spec_init = 13,
+    num_ax = 4
+    )
+
+
+specs, scram = rh.plot_histos(
+    specs=specs,
+    scram=scram,
+    x_lim=[1000,2000],
+    #x_lim = [7600,8200],
+    #x_line = [4560],
+    scram_method='cs',
+    cs_init = 14,
+    spec_init = 17,
+    num_ax = 4
+    )
+
+# XRS-LR
+specs, scram = rh.plot_histos(
+    specs=specs,
+    scram=scram,
+    x_lim=[6850, 8730],
+    #x_lim = [7600,8200],
+    #x_line = [4560],
+    scram_method='cs',
+    cs_init = 14,
     spec_init = 14,
     num_ax = 4
+    )
+
+
+
+################################################
+#
+#           Plot side-by-side
+#
+################################################
+
+# Modules
+from atomic_world.read_LLNL_EBIT.main import read_histos as rh
+
+# Enables automatic reloading of modules
+%reload_ext autoreload
+%autoreload 2
+
+# Feature 1
+specs, scram = rh.plot_side_by_side(
+    specs=specs,
+    scram=scram,
+    plt_keys = ['8.71 kV', '8.97 kV', '9.23 kV'],
+    x_lim=[[2740, 2850], [4500, 4580]],
+    scram_method='cs',
+    cs_init = 14,
+    x_line = [4560, 4543, 4531, 4513],
+    plt_scram = [True, False],
+    )
+
+# Feature 2
+specs, scram = rh.plot_side_by_side(
+    specs=specs,
+    scram=scram,
+    plt_keys = ['8.97 kV', '9.23 kV', '9.49 kV'],
+    x_lim=[[2740, 2850], [4500, 4580]],
+    scram_method='cs',
+    cs_init = 14,
+    x_line = [4560, 4543, 4531, 4513],
+    plt_scram = [True, False],
+    )
+
+# Feature 3
+specs, scram = rh.plot_side_by_side(
+    specs=specs,
+    scram=scram,
+    plt_keys = ['9.23 kV', '9.49 kV', '9.75 kV'],
+    x_lim=[[2740, 2850], [4500, 4580]],
+    scram_method='cs',
+    cs_init = 14,
+    x_line = [4560, 4543, 4531, 4513],
+    plt_scram = [True, False],
+    )
+
+# Feature 4
+specs, scram = rh.plot_side_by_side(
+    specs=specs,
+    scram=scram,
+    plt_keys = ['8.71 kV', '8.97 kV', '9.23 kV'],
+    x_lim=[[2450, 2550], [4500, 4580]],
+    scram_method='cs',
+    cs_init = 14,
+    x_line = [4560, 4543, 4531, 4513],
+    plt_scram = [True, False],
+    )
+
+# Feature 5
+specs, scram = rh.plot_side_by_side(
+    specs=specs,
+    scram=scram,
+    plt_keys = ['9.23 kV', '9.49 kV', '9.75 kV'],
+    x_lim=[[2450, 2550], [4500, 4580]],
+    scram_method='cs',
+    cs_init = 14,
+    x_line = [4560, 4543, 4531, 4513],
+    plt_scram = [True, False],
+    )
+
+# Feature 6
+specs, scram = rh.plot_side_by_side(
+    specs=specs,
+    scram=scram,
+    plt_keys = ['8.71 kV', '8.97 kV', '9.23 kV'],
+    #plt_keys = ['8.45 kV', '8.71 kV', '8.97 kV'],
+    x_lim=[[2600, 2700], [4500, 4580]],
+    scram_method='cs',
+    cs_init = 14,
+    x_line = [4560, 4543, 4531, 4513],
+    plt_scram = [True, False],
+    )
+
+# Feature 7
+specs, scram = rh.plot_side_by_side(
+    specs=specs,
+    scram=scram,
+    plt_keys = ['9.23 kV', '9.49 kV', '9.75 kV'],
+    x_lim=[[2600, 2700], [4500, 4580]],
+    scram_method='cs',
+    cs_init = 14,
+    x_line = [4560, 4543, 4531, 4513],
+    plt_scram = [True, False],
+    )
+
+# Feature 8
+specs, scram = rh.plot_side_by_side(
+    specs=specs,
+    scram=scram,
+    plt_keys = ['9.23 kV', '9.49 kV', '9.75 kV'],
+    x_lim=[[8900, 9150], [4500, 4580]],
+    scram_method='cs',
+    cs_init = 14,
+    x_line = [4560, 4543, 4531, 4513],
+    plt_scram = [True, False],
+    )
+
+# Feature 9
+specs, scram = rh.plot_side_by_side(
+    specs=specs,
+    scram=scram,
+    plt_keys = ['9.75 kV', '15(lowCB) kV', '15(highCB) kV'],
+    x_lim=[[8900, 9150], [4500, 4580]],
+    scram_method='cs',
+    cs_init = 14,
+    x_line = [4560, 4543, 4531, 4513],
+    plt_scram = [True, False],
+    )
+
+
+# Feature 10
+specs, scram = rh.plot_side_by_side(
+    specs=specs,
+    scram=scram,
+    plt_keys = ['8.71 kV', '8.97 kV', '9.23 kV'],
+    x_lim=[[3250, 3350], [4500, 4580]],
+    scram_method='cs',
+    cs_init = 14,
+    x_line = [4560, 4543, 4531, 4513],
+    plt_scram = [True, False],
+    )
+
+# Feature 11
+specs, scram = rh.plot_side_by_side(
+    specs=specs,
+    scram=scram,
+    plt_keys = ['9.23 kV', '9.49 kV', '9.75 kV'],
+    x_lim=[[3250, 3350], [4500, 4580]],
+    scram_method='cs',
+    cs_init = 14,
+    x_line = [4560, 4543, 4531, 4513],
+    plt_scram = [True, False],
+    )
+
+# Feature 12
+specs, scram = rh.plot_side_by_side(
+    specs=specs,
+    scram=scram,
+    plt_keys = ['9.75 kV', '15(lowCB) kV', '15(highCB) kV'],
+    x_lim=[[3250, 3350], [4500, 4580]],
+    scram_method='cs',
+    cs_init = 14,
+    x_line = [4560, 4543, 4531, 4513],
+    plt_scram = [True, False],
+    )
+
+# Feature 13
+specs, scram = rh.plot_side_by_side(
+    specs=specs,
+    scram=scram,
+    #plt_keys = ['8.71 kV', '8.97 kV', '9.23 kV'],
+    #plt_keys = ['9.23 kV', '9.49 kV', '9.75 kV'],
+    plt_keys = ['9.75 kV', '15(lowCB) kV', '15(highCB) kV'],
+    x_lim=[[4100, 4200], [4500, 4580]],
+    scram_method='cs',
+    cs_init = 14,
+    x_line = [4560, 4543, 4531, 4513],
+    plt_scram = [True, False],
+    )
+
+# Feature 14
+specs, scram = rh.plot_side_by_side(
+    specs=specs,
+    scram=scram,
+    #plt_keys = ['8.71 kV', '8.97 kV', '9.23 kV'],
+    #plt_keys = ['9.23 kV', '9.49 kV', '9.75 kV'],
+    plt_keys = ['9.75 kV', '15(lowCB) kV', '15(highCB) kV'],
+    x_lim=[[4550, 4650], [4500, 4580]],
+    scram_method='cs',
+    cs_init = 14,
+    x_line = [4560, 4543, 4531, 4513],
+    plt_scram = [True, False],
+    )
+
+# Feature 15
+specs, scram = rh.plot_side_by_side(
+    specs=specs,
+    scram=scram,
+    #plt_keys = ['8.71 kV', '8.97 kV', '9.23 kV'],
+    #plt_keys = ['9.23 kV', '9.49 kV', '9.75 kV'],
+    plt_keys = ['9.75 kV', '15(lowCB) kV', '15(highCB) kV'],
+    x_lim=[[4450, 4550], [4500, 4580]],
+    scram_method='cs',
+    cs_init = 14,
+    x_line = [4560, 4543, 4531, 4513],
+    plt_scram = [True, False],
     )
